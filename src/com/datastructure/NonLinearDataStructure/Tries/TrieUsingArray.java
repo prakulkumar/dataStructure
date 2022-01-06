d709aa1dab947e18f74357c30999abf2838c1b09
Prakul Kumar <KumarP1@bv.com>
2022-01-06 13:58:08 +0530

    Add Tries

---
+++ src/com/datastructure/NonLinearDataStructure/Tries/TrieUsingArray.java
@@ -0,0 +1,36 @@
+package com.datastructure.NonLinearDataStructure.Tries;
+
+public class TrieUsingArray {
+    public final int ALPHABET_SIZE = 26;
+
+    private class Node {
+        private char value;
+        private Node[] children = new Node[ALPHABET_SIZE];
+        private boolean isEndOfWord;
+
+        public Node(char value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "value=" + value + "isEndOfWord=" + isEndOfWord;
+        }
+    }
+
+    private Node root = new Node(' ');
+
+    public void insert(String word) {
+        var current = root;
+
+        for (var ch : word.toCharArray()) {
+            var index = ch - 'a';
+            if (current.children[index] == null)
+                current.children[index] = new Node(ch);
+
+            current = current.children[index];
+        }
+
+        current.isEndOfWord = true;
+    }
+}
---
+++ src/com/datastructure/NonLinearDataStructure/Tries/TrieUsingHashTable.java
@@ -0,0 +1,37 @@
+package com.datastructure.NonLinearDataStructure.Tries;
+
+import java.util.HashMap;
+
+public class TrieUsingHashTable {
+    public final int ALPHABET_SIZE = 26;
+
+    private class Node {
+        private char value;
+        private HashMap<Character, Node> children = new HashMap<>();
+        private boolean isEndOfWord;
+
+        public Node(char value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "value=" + value + "isEndOfWord=" + isEndOfWord;
+        }
+    }
+
+    private Node root = new Node(' ');
+
+    public void insert(String word) {
+        var current = root;
+
+        for (var ch : word.toCharArray()) {
+            if (current.children.get(ch) == null)
+                current.children.put(ch, new Node(ch));
+
+            current = current.children.get(ch);
+        }
+
+        current.isEndOfWord = true;
+    }
+}

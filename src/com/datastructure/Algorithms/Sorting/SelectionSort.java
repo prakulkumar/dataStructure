17ed8311314268678eb75a5d836cd0f5414ced90
Prakul Kumar <KumarP1@bv.com>
2022-01-09 14:48:12 +0530

    Add Insertion Sort

---
+++ src/com/datastructure/Algorithms/Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package com.datastructure.Algorithms.Sorting;
+
+public class SelectionSort {
+    public void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            var minIndex = getMinimumIndex(array, i);
+            swap(array, minIndex, i);
+        }
+    }
+
+    private int getMinimumIndex(int[] array, int index) {
+        var minIndex = index;
+        for (int j = index; j < array.length; j++)
+            if (array[j] < array[minIndex])
+                minIndex = j;
+        return minIndex;
+    }
+
+    private void swap(int[] array, int index1, int index2) {
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}

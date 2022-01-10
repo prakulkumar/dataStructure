ae0b1284917d245f577e1ea6d2313f4074f6e2e2
Prakul Kumar <KumarP1@bv.com>
2022-01-10 15:26:36 +0530

    Add Jump Search

---
+++ src/com/datastructure/Algorithms/Searching/JumpSearch.java
@@ -0,0 +1,23 @@
+package com.datastructure.Algorithms.Searching;
+
+public class JumpSearch {
+    public int search(int[] array, int target) {
+        int blockSize = (int) Math.sqrt(array.length);
+        int start = 0;
+        int next = blockSize;
+
+        while (start < array.length && array[next - 1] < target) {
+            start = next;
+            next += blockSize;
+            if (next > array.length)
+                next = array.length;
+        }
+
+        // Linear Search
+        for (int i = start; i < next; i++)
+            if (array[i] == target)
+                return i;
+
+        return -1;
+    }
+}

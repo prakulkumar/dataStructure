9931d052514ac2e2bf0a2ab9da499f3adc008c61
Prakul Kumar <KumarP1@bv.com>
2022-01-10 13:28:31 +0530

    Add Linear Search

---
+++ src/com/datastructure/Algorithms/Searching/LinearSearch.java
@@ -0,0 +1,11 @@
+package com.datastructure.Algorithms.Searching;
+
+public class LinearSearch {
+    public int search(int[] array, int target) {
+        for (int i = 0; i < array.length; i++)
+            if (array[i] == target)
+                return i;
+
+        return -1;
+    }
+}
